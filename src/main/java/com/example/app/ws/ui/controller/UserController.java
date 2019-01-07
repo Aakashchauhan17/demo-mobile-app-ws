a6c0af55b3e8aeea2ee711dc40e8d6cdc798964b
Aakash <aakash.chauhan1994@gmail.com>
2019-01-07 16:45:51 -0500

    Initial commit

---
+++ src/main/java/com/example/app/ws/ui/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.example.app.ws.ui.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.ws.service.UserService;
+import com.example.app.ws.shared.dto.UserDto;
+import com.example.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.example.app.ws.ui.model.response.UserRest;
+
+@RestController
+@RequestMapping("/users") // http://localhost:8080/users
+
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping
+	public String getUser()
+	{
+		return "get user was called";
+	}
+	
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails)
+	{
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		return returnValue;
+		// return null;
+	}
+	
+	@PutMapping
+	public String updateUser()
+	{
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser()
+	{
+		return "delete user was called";
+	}
+
+}
